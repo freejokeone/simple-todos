32d50efc8a901a8de623c639a62a28d206899b94
admine <wangningcumt@126.com>
2016-07-09 12:52:45 +0800

    wangning

--- imports/startup/accounts-config.js
+++
@@ -1,5 +0,0 @@
-import {Accounts} from 'meteor/accounts-base';
-
-Account.ui.config({
-	passwordSignupFields:'USERNAME_ONLY',
-});
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,50 +1,11 @@
-import {Meteor} from 'meteor/meteor';
-
 import {Template} from 'meteor/templating';
 
 import './body.html';
 
-import {ReactiveDict} from 'meteor/reactive-dict';
-
-import './task.js';
-
-import {Tasks} from '../api/tasks.js';
-
 Template.body.helpers({
-	tasks(){
-		const instance=Template.instance();
-		if (instance.state.get('hideCompleted')){
-			return Tasks.find({checked:{$ne:true}},{sort:{createAt:-1}});
-		}
-		return Tasks.find({},{sort:{createAt:-1}});
-	},
-	incomlpeteCount(){
-		return Tasks.find({checked:{$ne:true}}).count();
-	},
-});
-
-Template.body.onCreated(function bodyOnCreated(){
-	this.state=new ReactiveDict();
+	tasks:[
+		{text:'This is task 1'},
+		{text:'This is task 2'},
+		{text:'This is task 3'}
+	],
 })
-
-
-Template.body.events({
-	'submit .new-task'(event){
-		event.preventDefault();
-
-		const target=event.target;
-		const text=target.text.value;
-		Tasks.insert({
-			text,
-			createdAt:new Date(),
-			owner:Meteor.userId(),
-			username:Meteor.user().username,
-		});
-		//clear form
-		target.text.value="";
-	},
-	'change .hide-completed input'(event,instance){
-		instance.state.set('hideCompleted',event.target.checked);
-	},
-});
-
